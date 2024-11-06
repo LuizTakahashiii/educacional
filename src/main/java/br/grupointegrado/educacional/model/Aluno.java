a56ee8df86a7df7316891babd8e5a7cbcfbcd08e
LuizTakahashii <luiz.takahashi@grupointegrado.br>
2024-11-06 20:19:50 -0300

    Adicionar getters e setters na classe Aluno

--- src/main/java/br/grupointegrado/educacional/model/Aluno.java
+++ src/main/java/br/grupointegrado/educacional/model/Aluno.java
@@ -23,5 +23,44 @@ public class Aluno {
     @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
 
-    // Getters e Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
